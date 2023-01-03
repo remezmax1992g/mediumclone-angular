292e9fcf4b495b2e5ef32ecb94ed37fec07f64e1
Maxim Remez <remezmaxim1992@gmail.com>
2023-01-04 02:10:11 +0300

    create interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
-import {NgModule} from '@angular/core'
+import {isDevMode, NgModule} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
 import {AppComponent} from './app.component'
 import {AuthModule} from './auth/auth.module'
 import {AppRoutingModule} from './app-routing.module'
 import {StoreModule} from '@ngrx/store'
+import {StoreDevtoolsModule} from '@ngrx/store-devtools'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AuthModule, AppRoutingModule, StoreModule.forRoot({}, {})],
+  imports: [
+    BrowserModule,
+    AuthModule,
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
