90cc37a235398fffdb255db1ef36b842a239b66d
Maxim Remez <remezmaxim1992@gmail.com>
2023-01-04 02:32:03 +0300

    create reducer

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,9 +4,16 @@ import {ReactiveFormsModule} from '@angular/forms'
 
 import {RegisterComponent} from './components/register/register.component'
 import {AuthRoutingModule} from './auth-routing.module'
+import {StoreModule} from '@ngrx/store'
+import {reducer} from './store/reducers'
 
 @NgModule({
-  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    StoreModule.forFeature('auth', reducer),
+  ],
   declarations: [RegisterComponent],
   exports: [RegisterComponent],
 })
---
+++ src/app/auth/store/reducers.ts
@@ -0,0 +1,15 @@
+import {AuthStateInterface} from '../types/authState.interface'
+import {Action, createReducer, on} from '@ngrx/store'
+import {registerAction} from './actions/register.action'
+
+const initialState: AuthStateInterface = {
+  isSubmitting: false,
+}
+const authReducer = createReducer(
+  initialState,
+  on(registerAction, (state): AuthStateInterface => ({...state, isSubmitting: true}))
+)
+
+export function reducer(state: AuthStateInterface, action: Action) {
+  return authReducer(state, action)
+}
